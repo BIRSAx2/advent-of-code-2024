24c97331b4b63dda47fc172f7adec9f169cc28f6
Mouhieddine Sabir <mouhieddine.sabir@multisafepay.com>
2024-12-02 23:30:52 +0100

    Add Day 1 solutions

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -2,27 +2,29 @@ defmodule AdventOfCode.Day01 do
   defp parse(args) do
     args
     |> String.split("\n", trim: true)
-    |> Enum.map(&String.split(&1, ~r/\s+/))
-    |> Enum.map(fn row -> Enum.map(row, &String.to_integer/1) end)
-    |> Enum.reduce([[], []], fn [a, b], [list_a, list_b] ->
-      [[a | list_a], [b | list_b]]
+    |> Enum.map(fn line ->
+      line
+      |> String.split(~r/\s+/)
+      |> Enum.map(&String.to_integer/1)
+      |> List.to_tuple()
     end)
+    |> Enum.unzip()
   end
 
   def part1(args) do
-    args
-    |> parse()
-    |> then(fn [list_a, list_b] -> [Enum.sort(list_a), Enum.sort(list_b)] end)
-    |> Enum.zip()
-    |> Enum.reduce(0, fn {a, b}, acc -> acc + abs(a - b) end)
+    {list_a, list_b} = parse(args)
+
+    list_a
+    |> Enum.sort()
+    |> Enum.zip(Enum.sort(list_b))
+    |> Enum.map(fn {a, b} -> abs(a - b) end)
+    |> Enum.sum()
   end
 
   def part2(args) do
-    args
-    |> parse()
-    |> then(fn [list_a, list_b] -> [list_a, Enum.frequencies(list_b)] end)
-    |> then(fn [list_a, freq] ->
-      Enum.reduce(list_a, 0, fn a, acc -> acc + a * Map.get(freq, a, 0) end)
-    end)
+    {list_a, list_b} = parse(args)
+    freq_map = Enum.frequencies(list_b)
+
+    Enum.reduce(list_a, 0, fn a, acc -> acc + a * Map.get(freq_map, a, 0) end)
   end
 end
