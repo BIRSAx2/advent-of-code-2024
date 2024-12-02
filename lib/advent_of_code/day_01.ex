3801d019df36fdc39b73d3fbb28a7db9515ba2a3
Mouhieddine Sabir <mouhieddine.sabir@multisafepay.com>
2024-12-02 23:25:42 +0100

    Initial commit

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,7 +1,28 @@
 defmodule AdventOfCode.Day01 do
-  def part1(_args) do
+  defp parse(args) do
+    args
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.split(&1, ~r/\s+/))
+    |> Enum.map(fn row -> Enum.map(row, &String.to_integer/1) end)
+    |> Enum.reduce([[], []], fn [a, b], [list_a, list_b] ->
+      [[a | list_a], [b | list_b]]
+    end)
   end
 
-  def part2(_args) do
+  def part1(args) do
+    args
+    |> parse()
+    |> then(fn [list_a, list_b] -> [Enum.sort(list_a), Enum.sort(list_b)] end)
+    |> Enum.zip()
+    |> Enum.reduce(0, fn {a, b}, acc -> acc + abs(a - b) end)
+  end
+
+  def part2(args) do
+    args
+    |> parse()
+    |> then(fn [list_a, list_b] -> [list_a, Enum.frequencies(list_b)] end)
+    |> then(fn [list_a, freq] ->
+      Enum.reduce(list_a, 0, fn a, acc -> acc + a * Map.get(freq, a, 0) end)
+    end)
   end
 end
--- lib/mix/tasks/d01.p1.ex
+++ lib/mix/tasks/d01.p1.ex
@@ -3,19 +3,9 @@ defmodule Mix.Tasks.D01.P1 do
 
   import AdventOfCode.Day01
 
-  @year 2024
   @shortdoc "Day 01 Part 1"
   def run(args) do
-    _input = AdventOfCode.Input.get!(1, @year)
-
-    input = """
-    3   4
-    4   3
-    2   5
-    1   3
-    3   9
-    3   3
-    """
+    input = AdventOfCode.Input.get!(1, 2024)
 
     if Enum.member?(args, "-b"),
       do: Benchee.run(%{part_1: fn -> input |> part1() end}),
